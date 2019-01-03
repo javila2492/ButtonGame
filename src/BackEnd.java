96b26985480e4f97b01cafb87c0fd9b3d0a74dc8
BT_1N3_11 <HELP>
2019-01-03 09:52:39 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BackEnd
 {
+    public void readScores(String name) throws IOException
+    {
+        String scoreList = "";
+        int places = 1;
+        BufferedReader in = new BufferedReader(new FileReader(name));
+
+        String line;
+        while((line = in.readLine()) != null)
+        {
+            scoreList += places + ": " +
+        }
+        in.close();
+    }
 }
--- src/GameCode.java
+++ src/GameCode.java
@@ -1,3 +1,4 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -5,32 +6,59 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import static java.lang.System.nanoTime;
 
 public class GameCode extends Application
 {
+    public int a = 0;
+    public static Label scores;
+    public Label label;
+    public Button button;
+    public HBox box;
+    public boolean on = true;
+    long timer;
+
     @Override
-    public void start(Stage primaryStage) throws Exception
+    public void start(Stage primaryStage)
     {
-        int a = 0;
-
         primaryStage.setTitle("Button Clicker");
-        Label label = new Label("Clicked " + a + " times");
-        Button button = new Button("Click Me!");
+        label.setText("Clicked " + a + " times");
+        button.setText("Click Me!");
 
-        button.setOnAction(value -> a++, value -> label.setText("Clicked " + a + " times"));
+        button.setOnAction(value ->
+        {
+            if(on)
+            {
+                a++;
+                label.setText("Clicked " + a + " times");
+            }
+        });
 
         button.setMaxSize(100, 100);
-        HBox hbox = new HBox(button, label);
-        Scene scene = new Scene(hbox, 200, 150);
+        box = new HBox(button, label);
+        Scene scene = new Scene(box, 200, 150);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
-    public void labelChange(Label c, int b)
-    {
-        b++;
-        c.setText("Clicked " + b + " times");
+        timer = System.nanoTime() + 1000000000L;
+        new AnimationTimer() {
+            public void handle(long now)
+            {
+                if (now > timer)
+                {
+                    timer = now + 1000000000L;
+                    on = !on;
+                }
+                if (!on)
+                {
+                    button.setText("Don't Click");
+                }
+                else
+                {
+                    button.setText("Click Me!");
+                }
+                label.setText("Score:" + Integer.toString(a));
+            }}.start();
     }
 
     public static void main(String[] args)
