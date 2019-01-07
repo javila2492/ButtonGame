0f4a4cfb59d3f10dd02dbbb0c4485082dcdc2c03
Johan <javila2492@bths.edu>
2019-01-06 19:46:39 -0500

    AAAAAAAAAAAAAAAAAAaa

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,20 +1,60 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class BackEnd
 {
-    public void readScores() throws IOException
+    public static ArrayList<String> readScore()
     {
-        String scoreList = "";
-        int places = 1;
-        BufferedReader in = new BufferedReader(new FileReader("scores.txt"));
-
-        String line;
-        while((in.readLine()) != null)
+        Path path = Paths.get("score.csv");
+        ArrayList<String> scores = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(path,
+                StandardCharsets.US_ASCII))
         {
-            scoreList += places + ": " + in.readLine();
+            String line = br.readLine();
+            while (line != null)
+            {
+                scores.add(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe)
+        {
+            ioe.printStackTrace();
         }
-        in.close();
+
+
+        return scores;
+    }
+    public static void updateScore(int points)
+    {
+        ArrayList<String> scores = readScore();
+        for(int i = 0; i<scores.size();  i++)
+        {
+            if(points > Integer.parseInt(scores.get(i)))
+            {
+                try
+                {
+                    scores.add(i,""+points);
+                    BufferedWriter out = new BufferedWriter(new FileWriter("src/sample/score.txt", false));
+                    for(int j = 0;j < scores.size() - 1; j++)
+                    {
+                        out.write(scores.get(j) + "\n");
+                    }
+                    out.close();
+                }
+                catch (IOException e)
+                {
+                    System.out.println("exception occurred" + e);
+                }
+                break;
+            }
+        }
     }
 }
--- src/GameCode.java
+++ src/GameCode.java
@@ -1,48 +1,66 @@
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.util.Duration;
 
 
 public class GameCode extends Application
 {
     public int a = 0;
+
+    @FXML
     public static Label scores;
     public Label label;
     public Button button;
     public HBox box;
+
     public boolean on = true;
     long timer;
+    boolean loss = false;
+    boolean over = false;
 
     @Override
     public void start(Stage primaryStage)
     {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Button Clicker");
-        label.setText("Clicked " + a + " times");
-        button.setText("Click Me!");
+        Label label = new Label("Score: " + a);
 
+        Button button  = new Button("Click Me!");
         button.setOnAction(value ->
         {
+            if(loss)
+            {
+                a--;
+                label.setText("Score: " + a);
+            }
             if(on)
             {
                 a++;
-                label.setText("Clicked " + a + " times");
+                label.setText("Score: " + a);
             }
         });
 
-        button.setMaxSize(100, 100);
+        button.setMaxSize(150, 120);
         box = new HBox(button, label);
-        Scene scene = new Scene(box, 200, 150);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(box, 400, 150);
+        primaryStage.setScene(new Scene(root, scene, 300, 150));
 
         timer = System.nanoTime() + 1000000000L;
         new AnimationTimer() {
             public void handle(long now)
             {
+                if(over)
+                    return;
                 if (now > timer)
                 {
                     timer = now + 1000000000L;
@@ -51,13 +69,26 @@ public class GameCode extends Application
                 if (!on)
                 {
                     button.setText("Don't Click");
+                    loss = true;
                 }
                 else
                 {
                     button.setText("Click Me!");
+                    loss = false;
                 }
                 label.setText("Score:" + Integer.toString(a));
             }}.start();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10000), actionEvent -> end()));
+        timeline.play();
+        primaryStage.show();
+    }
+
+    public void end()
+    {
+        button.setText("Game Over!");
+        over = true;
+        button.setStyle("-fx-background-color: white; -fx-font-family: cursive; -fx-font-weight: bold;");
     }
 
     public static void main(String[] args)
